b70103dbb2ae65261c6526640d44e13bec6ea8d1
Daniel Demerin <ddemerin.code@gmail.com>
2020-10-27 20:50:41 -0400

    Marinara Sauce exercise

--- initialTest.js
+++ initialTest.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function MarinaraSauce(name){
+    console.log(`"Hey ${name}! This is my marinara sauce recipe!`);
+    console.log("Heat a medium-large saucepan over medium heat.");
+    console.log("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
+    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+    console.log("Viola! Marinara Sauce! Buon appetito!")
+}
+
+MarinaraSauce("Brian");
