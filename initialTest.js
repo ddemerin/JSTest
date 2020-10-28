47ae3306339b1fdb37074758d8cb6568aea7c386
Daniel Demerin <ddemerin.code@gmail.com>
2020-10-27 21:35:00 -0400

    teaching students Template Literals

--- initialTest.js
+++ initialTest.js
@@ -1,11 +1,15 @@
-function MarinaraSauce(name){
-    console.log(`"Hey ${name}! This is my marinara sauce recipe!`);
+function MarinaraSauce(name, measurementOne, measurementTwo){
+    console.log(`Hey ${name}! This is my marinara sauce recipe!`);
     console.log("Heat a medium-large saucepan over medium heat.");
-    console.log("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes");
-    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log(`Add ${measurementOne} tspns of oil and 5 carlic cloves, cook until golden, about ${measurementOne} minutes`);
+    console.log(`Add ${measurementTwo} cup water, ${measurementOne} cans of crushed tomates, and salt and season with black pepper to taste.`);
     console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
-    console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+    console.log(`Stir in ${measurementTwo} cup roughly chopped fresh basil, salt and pepper as needed.`);
     console.log("Viola! Marinara Sauce! Buon appetito!")
+    console.log("");
 }
 
-MarinaraSauce("Brian");
+let cookOne = "Elle";
+
+MarinaraSauce(cookOne, "2","1/4");
+MarinaraSauce("Florence", "5", "1.25");
